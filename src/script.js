3a3f475aa5eab02edd651056f4d1f16d55084340
Ameer <ameerapex496@gmail.com>
2023-12-28 03:11:13 +0100

    added indez -z for close btn wrap + added logic open and close navbar

--- src/script.js
+++ src/script.js
@@ -1,12 +1,17 @@
 // selecting element
 var navOpenBtn = document.getElementsByClassName("navbar-Bars-Wrap")[0];
 var navCloseBtn = document.getElementsByClassName("nav-Close-Btn")[0];
+var navlinkEl = document.getElementsByClassName("navlinks")[0];
 // gelobal varibales
 // functions
 // Open Nav
-var openNav = function () { };
+var openNav = function () {
+    navlinkEl.classList.remove("active");
+};
 // close Nav
-var closeNav = function () { };
+var closeNav = function () {
+    navlinkEl.classList.add("active");
+};
 // eventlinsters
 navOpenBtn.addEventListener("click", openNav);
 navCloseBtn.addEventListener("click", closeNav);
--- src/script.ts
+++ src/script.ts
@@ -1,16 +1,21 @@
 // selecting element
 const navOpenBtn = document.getElementsByClassName("navbar-Bars-Wrap")[0];
 const navCloseBtn = document.getElementsByClassName("nav-Close-Btn")[0];
+const navlinkEl = document.getElementsByClassName("navlinks")[0];
 
 // gelobal varibales
 
 // functions
 
 // Open Nav
-const openNav = () => {};
+const openNav = () => {
+  navlinkEl.classList.remove("active");
+};
 
 // close Nav
-const closeNav = () => {};
+const closeNav = () => {
+  navlinkEl.classList.add("active");
+};
 
 // eventlinsters
 navOpenBtn.addEventListener("click", openNav);
