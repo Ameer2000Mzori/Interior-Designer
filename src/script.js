a2e11ee355f53606cdeb0e810d75f35d2637648b
Ameer <ameerapex496@gmail.com>
2023-12-28 02:53:54 +0100

    selected elemets + added comment needed for coding

--- src/script.js
+++ src/script.js
@@ -1,1 +1,8 @@
-console.log("hallo world");
+// selecting element
+var navOpenBtn = document.getElementsByClassName("navbar-Bars-Wrap")[0];
+var navCloseBtn = document.getElementsByClassName("nav-Close-Btn")[0];
+// gelobal varibales
+// functions
+// eventlinsters
+navOpenBtn.addEventListener("click", openNav);
+navCloseBtn.addEventListener("click", closeNav);
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,11 @@
-console.log("hallo world");
+// selecting element
+const navOpenBtn = document.getElementsByClassName("navbar-Bars-Wrap")[0];
+const navCloseBtn = document.getElementsByClassName("nav-Close-Btn")[0];
+
+// gelobal varibales
+
+// functions
+
+// eventlinsters
+navOpenBtn.addEventListener("click", openNav);
+navCloseBtn.addEventListener("click", closeNav);
